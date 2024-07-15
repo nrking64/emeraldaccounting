a2bec076e82b2b114eb5627e385eb9784937d7d4
Cristian Milatinov <milatinovcristian@gmail.com>
2024-07-15 16:21:39 -0400

    Add tailwind + header, footer layout + format code

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header>
+        <nav>header</nav>
+    </header>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{html,ts,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#50C878',
+                'bg-accent': '#ACE1AF'
+            }
+        }
+    },
+    plugins: []
+};
+
